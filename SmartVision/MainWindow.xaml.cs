d818ae91e5f9579ebc4cb47d604a1357bf30e597
rami ismail <ismail.ismailrami@gmail.com>
2016-02-13 21:32:37 +0100

    first commit

---
+++ SmartVision/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+
+namespace SmartVision
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Title = "Read file";
+        }
+
+        private void setOpen(object sender, RoutedEventArgs e)
+        {
+            List<Person> list = new List<Person>();
+            Microsoft.Win32.OpenFileDialog opd = new Microsoft.Win32.OpenFileDialog();
+            opd.DefaultExt=".CSV";
+            opd.Filter = "CSV file (*.csv)|*.CSV|txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            Nullable<bool> result = opd.ShowDialog();
+            if(result==true)
+            {
+                StreamReader fileReader = new StreamReader(opd.FileName, false);
+                while (fileReader.Peek() != -1)
+                {
+                    string fileRow = fileReader.ReadLine();
+                    string[] data= fileRow.Split(',');
+                    Person person = new Person(data[0].Trim('\"'), data[1].Trim('\"'), data[2].Trim('\"'), Int32.Parse(data[3].Trim('\"')));
+                    Console.Write(person.ToString());
+                    list.Add(person);
+                }
+                dataGrid.ItemsSource = list;
+
+            }           
+        }
+    }
+}
---
+++ SmartVision/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartVision
+{
+    class Person
+    {
+        public string name { get; set; }
+        public string email { get; set; }
+        public string gender { get; set; }
+        public int id { get; set; }
+        public Person(string name , string email,string gender, int id)
+        {
+            this.name = name;
+            this.email = email;
+            this.gender = gender;
+            this.id = id;
+        }
+        public string ToString()
+        {
+            return this.name + " " + this.email;
+        }
+
+    }
+}
